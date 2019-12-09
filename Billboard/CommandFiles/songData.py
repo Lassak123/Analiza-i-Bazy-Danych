2638e3e11771191a4b76a8d267a466932de359d7
Kevaen <kuba640@gmail.com>
2019-12-09 14:42:25 +0100

    Bbillboard update

--- Billboard/CommandFiles/songData.py
+++ Billboard/CommandFiles/songData.py
@@ -10,6 +10,10 @@ df = pd.read_csv("./../OriginalData/billboard.csv")
 #Get only intresting information
 df1 = df[['artist.inverted','track', 'time','genre']]
 
+for index, row in df1.iterrows():
+    time = re.findall('\d+',row['time'])
+    time_sec = int(time[0])*30+int(time[1])
+    row['time']=time_sec
 #Sort data
 df1 = df1.sort_values(ascending=True, by=["artist.inverted","track","time","genre"])
 
